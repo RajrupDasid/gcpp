9958afc4f7ec064c2f60ba00a9a57ceed7eb5c3b
RajrupDasid <rajrupdasid@outlook.com>
2021-12-25 15:40:52 +0530

    gnu gcc learning started

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; //decleared std on global level for ease
+int main()
+{
+	cout<<"Manjaro Linux"<<endl;// new line statement 
+	cout<<"openSUSE Linux"<<endl;
+	cout<<"Hello World"<<endl;//new line statement
+	cout<<"How are you today?"<<endl;//new line statement 
+	return 0;
+}
